6633aa0e841341b6f98700457c8723f503435253
003-demo <rohankabir061@gmail.com>
2025-06-18 23:27:13 +0600

    s

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const articlesCollections = client
       .db("assignment-11")
@@ -47,6 +47,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/articles/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await articlesCollections.findOne(query);
+      res.send(result);
+    });
+
     app.delete("/articles/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -62,7 +69,7 @@ async function run() {
     // });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
